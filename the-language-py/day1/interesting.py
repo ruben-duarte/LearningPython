12939602f2d565b1804c6c517532063646f7f95a
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2024-09-12 16:55:04 -0500

    checking features of python lists, tuples and strings

--- the-language-py/day1/interesting.py
+++ the-language-py/day1/interesting.py
@@ -83,3 +83,181 @@
 """
     python utiliza indentacion, tener cuidado con espacios y tabs. preferir espacios.
 """
+
+# >>> a = [1,2,3*
+# ...
+# ... ]
+#   File "<stdin>", line 3
+#     ]
+#     ^
+# SyntaxError: invalid syntax
+# >>> a = [1,2,3]
+# >>> a
+# [1, 2, 3]
+# >>> # sintaxis consistente para listas, strings
+# >>> len(a)
+# 3
+# >>> a + [2,4]
+# [1, 2, 3, 2, 4]
+# >>> a = [1,2,3]
+# >>> b = a
+# >>> a
+# [1, 2, 3]
+# >>> b
+# [1, 2, 3]
+# >>> a[0]
+# 1
+# >>> a[0] = 7
+# >>> a
+# [7, 2, 3]
+# >>> b
+# [7, 2, 3]
+# >>> #al modificar a se modifico b, b = a no es una copia, la lista es mutable
+# >>> b = a[:]
+# >>> # ahora si es una copia, antes ambas variables apuntaban al mismo objeto
+# >>> a == b
+# True
+# >>> a
+# [7, 2, 3]
+# >>> a[1:3]
+# [2, 3]
+# >>> a[:-1]
+# [7, 2]
+# >>> for num in a: print(num)
+# ...
+# 7
+# 2
+# 3
+# >>> 3 in a
+# True
+# >>> a.append(4)
+# >>> a
+# [7, 2, 3, 4]
+# >>> None
+# >>> #append no regresa nada sino modifica en sitio
+
+# >>> a.pop(0)
+# 7
+# >>> a
+# [2, 3, 4]
+# >>> del a
+# >>> a
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'a' is not defined
+# >>> #borra la definicion de a
+# >>> a = [1,2,3]
+# >>> del a[1]
+# >>> a
+# [1, 3]
+# >>> b = 14
+# >>> a = b
+# >>>
+# >>> del a
+# >>> a
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'a' is not defined
+# >>> b
+# 14
+# >>> #sorting
+# >>> a = [2,5,3,4,]
+# >>> sorted(a)
+# [2, 3, 4, 5]
+# >>> a
+# [2, 5, 3, 4]
+# >>> a.sort()
+# >>> a
+# [2, 3, 4, 5]
+# >>> sorted(a, reverse=True)
+# [5, 4, 3, 2]
+# >>> #argumentos opcionales en sorted genera una nueva copia
+# >>> #sobreescribir
+# >>> a = sorted(a)
+# >>> a
+# [2, 3, 4, 5]
+# >>> #custom sorted
+# >>> a = ['cc','aaa','e']
+# >>> sorted(a)
+# ['aaa', 'cc', 'e']
+# >>> # sort por longitud, two argument competitor technique en vez una key function en la lista original
+# >>> len
+# <built-in function len>
+# >>> a
+# ['cc', 'aaa', 'e']
+# >>> sorted(a, key=len)
+# ['e', 'cc', 'aaa']
+# >>> a[1] = 'aaaau'
+# >>> #sort por ultimo caracter
+# >>> def last(s): return s[-1]
+# ...
+# >>> last
+# <function last at 0x0000018901E45300>
+# >>> sorted(a, key=last)
+# ['cc', 'e', 'aaaau']
+# >>> sorted(a, key=last, reverse=True)
+# ['aaaau', 'e', 'cc']
+# >>> #crear un string de una lista
+# >>> ';'.join(a)
+# 'cc;aaaau;e'
+# >>> '\n'.join(a)
+# 'cc\naaaau\ne'
+# >>> b = ':'.join(a)
+# >>> b
+# 'cc:aaaau:e'
+# >>> b.split(':')
+# ['cc', 'aaaau', 'e']
+# >>> result = []
+# >>> for s in a: result.append(s)
+# ...
+# >>> result
+# ['cc', 'aaaau', 'e']
+# >>> range(14)
+# range(0, 14)
+# >>> for i in range(4): print(i)
+# ...
+# 0
+# 1
+# 2
+# 3
+# >>> #for loop en una lista no se puede modificar la estructura de la lista o su longitud mientras se recorre
+# >>> #tupla
+# >>> (1,2,3)
+# (1, 2, 3)
+# >>> #inmutable
+# >>> a = (1,2,3)
+# >>> len(a)
+# 3
+# >>> a[1*
+# ...
+# ...
+# ... ]
+#   File "<stdin>", line 4
+#     ]
+#     ^
+# SyntaxError: invalid syntax
+# >>> a[1]
+# 2
+# >>> a[1] = 5
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: 'tuple' object does not support item assignment
+# >>> #sort de una forma y luego de otra usar tuplas
+
+# >>> a = [(1,"b"),(2,"a")]
+# >>> #primero ordena por el primer item, si son iguales por el segundo
+# >>> sorted(a)
+# [(1, 'b'), (2, 'a')]
+# >>> a
+# [(1, 'b'), (2, 'a')]
+# >>> a = [(1,"b"),(2,"a"),(1,"a")]
+# >>> sorted(a)
+# [(1, 'a'), (1, 'b'), (2, 'a')]
+# >>> (x,y) = (1,2)
+# >>> s
+# 'e'
+# >>> x
+# 1
+# >>> y
+# 2
+# >>>
