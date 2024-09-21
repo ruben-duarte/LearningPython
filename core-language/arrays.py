65939da565d14449ec606dc4981c82d52b7f1a7f
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2024-09-21 14:04:58 -0500

    studying python in deptH

---
+++ core-language/arrays.py
@@ -0,0 +1,10 @@
+#arrays
+#apple stock prices AAPPL
+AAPPL_stock_prices = [298,305,320,301,290]
+                   #day 1   2   3   4  5
+                #index  0   1   2   3  4
+AAPPL_stock_prices[1]
+
+print("price on day 3")
+print(AAPPL_stock_prices[2], "USD")
+
