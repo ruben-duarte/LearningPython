4da999617d5dd08013dd6f6d630910871b07d056
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2024-09-25 11:01:10 -0500

    exploring arrays in python

--- core-language/arrays.py
+++ core-language/arrays.py
@@ -6,5 +6,40 @@ AAPPL_stock_prices = [298,305,320,301,290]
 AAPPL_stock_prices[1]
 
 print("price on day 3")
-print(AAPPL_stock_prices[2], "USD")
+print(AAPPL_stock_prices[2], "USD") #O(1)
 
+print("price on day 5")
+for index in range(len(AAPPL_stock_prices)):  #O(n)
+    if index == 4:
+        print(AAPPL_stock_prices[index])
+
+print("on which day price was 305")      #O(n)
+for index in range(len(AAPPL_stock_prices)):
+   if AAPPL_stock_prices[index] == 305:
+       print(index + 1)
+
+print("All the prices")
+for price in AAPPL_stock_prices:
+    print(price, end=" - ")
+
+print("insert price 500 in day 2") #O(n)
+AAPPL_stock_prices.insert(1,500)
+print(AAPPL_stock_prices)
+
+print("delet prices of last day")
+AAPPL_stock_prices.remove(290)
+print(AAPPL_stock_prices)
+print(len(AAPPL_stock_prices)-1)
+
+print("="*7)
+stock_prices = [price for price in range(1,40,10)]
+print(stock_prices)
+stock_names = ['APL','GOOG','AMZN','AIRBUS']
+
+stock_data = []
+for index in range(len(stock_names)):
+    data = dict()
+    data["ticker"] = stock_names[index]
+    data["price"] = stock_prices[index]
+    stock_data.append(data)
+print(stock_data)
